f454878efe4a8172706f90dc39e7dda2eeaf6c56
unknown <prasanna347306@gmail.com>
2022-06-26 02:50:24 +0530

    node_modules be gone!

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,13 @@
+const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed()
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    '0xc2311EC124552dd383fea48626BA9A9355A68B13',
+    '1000000000000000000000'
+  )
+};

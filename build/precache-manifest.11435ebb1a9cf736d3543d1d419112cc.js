a10731acfff53178837e1048cc59ee2e5c09bd28
Prasanna-39 <74823833+Prasanna-39@users.noreply.github.com>
2022-06-26 12:17:28 +0530

    Add files via upload

---
+++ build/precache-manifest.11435ebb1a9cf736d3543d1d419112cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7daa6563c47860fe68a",
+    "url": "/ETH-WALLET_BLOCKSHAIN-DAPP/static/css/main.730d7875.chunk.css"
+  },
+  {
+    "revision": "f7daa6563c47860fe68a",
+    "url": "/ETH-WALLET_BLOCKSHAIN-DAPP/static/js/main.f7daa656.chunk.js"
+  },
+  {
+    "revision": "922a04c8c69949045447",
+    "url": "/ETH-WALLET_BLOCKSHAIN-DAPP/static/css/1.4e81f23b.chunk.css"
+  },
+  {
+    "revision": "922a04c8c69949045447",
+    "url": "/ETH-WALLET_BLOCKSHAIN-DAPP/static/js/1.922a04c8.chunk.js"
+  },
+  {
+    "revision": "c1ed5fb9715aae36d35f",
+    "url": "/ETH-WALLET_BLOCKSHAIN-DAPP/static/js/runtime~main.c1ed5fb9.js"
+  },
+  {
+    "revision": "dfbbfbb7c307e697e813cb79d34b15ad",
+    "url": "/ETH-WALLET_BLOCKSHAIN-DAPP/static/media/dai-logo.dfbbfbb7.png"
+  },
+  {
+    "revision": "a0563ead1c2a37b13293b1a5204b84bb",
+    "url": "/ETH-WALLET_BLOCKSHAIN-DAPP/index.html"
+  }
+];
